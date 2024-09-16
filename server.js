4885cb10632442c34b8936acfe8b941b42a44289
Veeresh Hiremath <hiremathv2210@gmail.com>
2024-09-15 20:02:18 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,33 +1,27 @@
 const express = require('express');
 const app = express();
+
+const fs = require('fs');
+const path = require('path');
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 70
-    },
-    {
-        title: 'Rent',
-        budget: 500
-    },
-    {
-        title: 'Groceries',
-        budget: 120
-    },
-    ]
-};
+app.get('/budget', (req, res) =>{
+    
+    fs.readFile('./mybudget.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading mybudget.json:', err);
+            return res.status(404).send('myBudget file are not found');
+        }
+        const myBudget = JSON.parse(data);
+        res.json(myBudget);
+    })
+});
 
 
 app.get('/hello', (req, res) =>{
     res.send('Hello World!');
-});
-
-app.get('/budget', (req, res) =>{
-    res.json(budget);
 });
 
 app.listen(port, () =>{
